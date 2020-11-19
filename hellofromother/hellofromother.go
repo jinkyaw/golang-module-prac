fc29370843457e2e3e5eddad0077a54f109f7156
Ajinkya Wankhade <ajinkya.wankhade@globant.com>
2020-11-20 01:34:26 +0530

    initial commit

---
+++ hellofromother/hellofromother.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+    "example.com/greetings"
+)
+
+func main() {
+	// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+
+	// a slice of names
+	names := []string {"Chetna", "Aai", "Baba"}
+	
+    // Get a greeting message and print it.
+	messages, err := greetings.Hellos(names)
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	
+	// fmt.Println(messages)
+
+	// looping through messages to print each one
+	for _, message := range messages {
+        fmt.Println(message)
+    }
+}
